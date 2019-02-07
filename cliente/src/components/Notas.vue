b94ff70d236ca72e232a6193d69ef90b63c10f76
tenlla2 <tenlla2@gmail.com>
2019-02-07 09:15:22 +0100

    actu

--- cliente/src/components/Notas.vue
+++ cliente/src/components/Notas.vue
@@ -97,8 +97,7 @@
           </li>
         </transition-group>
       </ul>
-    </div>
-    <div>
+      <div>
       <beautiful-chat
         :participants="participants"
         :titleImageUrl="titleImageUrl"
@@ -116,6 +115,8 @@
         :messageStyling="messageStyling"
       />
     </div>
+    </div>
+    
   </div>
 </template>
 
@@ -153,12 +154,17 @@ export default {
       if (datos.estado) {
         this.estado = true;
         this.creador = datos.nick;
+        this.messageList.push(JSON.parse(datos.ChatUsuarios));
       }
     },
 
+    UsuariosChat: function(data){
+      this.participants= JSON.parse(data);
+    },
     Inicio: function(notas) {
       this.tareas = JSON.parse(notas);
-    }
+    },
+
   },
   data() {
     return {
@@ -167,9 +173,45 @@ export default {
       nombre: "",
       busqueda: "",
       nick: "",
+      cont:1,
       estado: false,
-      creador: ""
-    };
+      creador: "",
+      participants: [], // the list of all the participant of the conversation. `name` is the user name, `id` is used to establish the author of a message, `imageUrl` is supposed to be the user avatar.
+      titleImageUrl: 'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png',
+      messageList: [
+          { type: 'text', author: `me`, data: { text: `Say yes!` } },
+          { type: 'text', author: `user1`, data: { text: `No.` } }
+      ], // the list of the messages to show, can be paginated and adjusted dynamically
+      newMessagesCount: 0,
+      isChatOpen: false, // to determine whether the chat window should be open or closed
+      showTypingIndicator: '', // when set to a value matching the participant.id it shows the typing indicator for the specific user
+      colors: {
+        header: {
+          bg: '#4e8cff',
+          text: '#ffffff'
+        },
+        launcher: {
+          bg: '#4e8cff'
+        },
+        messageList: {
+          bg: '#ffffff'
+        },
+        sentMessage: {
+          bg: '#4e8cff',
+          text: '#ffffff'
+        },
+        receivedMessage: {
+          bg: '#eaeaea',
+          text: '#222222'
+        },
+        userInput: {
+          bg: '#f4f7f9',
+          text: '#565867'
+        }
+      }, // specifies the color scheme for the component
+      alwaysScrollToBottom: false, // when set to true always scrolls the chat to the bottom when new events are in (new message, user starts typing...)
+      messageStyling: true // enables *bold* /emph/ _underline_ and such (more info at github.com/mattezza/msgdown)
+    }
   },
   methods: {
     handleFilePondInit: function() {
@@ -209,6 +251,27 @@ export default {
     prioridad: function(index, n) {
       this.tareas[index].prioridad = n;
       this.$socket.emit("Notas", JSON.stringify(this.tareas));
+    },
+    //chat
+    sendMessage (text) {
+      if (text.length > 0) {
+        this.newMessagesCount = this.isChatOpen ? this.newMessagesCount : this.newMessagesCount + 1;
+        this.onMessageWasSent({ author: 'me', type: 'text', data: { text } });
+        this.$socket.emit("Mensaje", JSON.stringify({ author: this.creador, type: 'text', data: { text } }));
+      }
+    },
+    onMessageWasSent (message) {
+      // called when the user sends a message
+      this.messageList = [ ...this.messageList, message ]
+    },
+    openChat () {
+      // called when the user clicks on the fab button to open the chat
+      this.isChatOpen = true
+      this.newMessagesCount = 0
+    },
+    closeChat () {
+      // called when the user clicks on the botton to close the chat
+      this.isChatOpen = false
     }
   },
 
